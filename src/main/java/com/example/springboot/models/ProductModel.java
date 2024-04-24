93ecba8a1d4db1d3db1a953ca42527cc5cf8884e
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-04-23 21:25:41 -0300

    configurando model pt 4

--- src/main/java/com/example/springboot/models/ProductModel.java
+++ src/main/java/com/example/springboot/models/ProductModel.java
@@ -1,15 +1,24 @@
 package com.example.springboot.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.UUID;
 
 @Entity
 @Table(name = "TB_PRODUCTS")
 public class ProductModel implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID idProduct;
+    private String name;
+    private BigDecimal value;
+
 
 }
